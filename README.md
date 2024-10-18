09c9cecd7ba053c3b4267f231f17c50273a93e1c
s-rafiq <126106104+s-rafiq@users.noreply.github.com>
2024-10-18 09:37:15 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal Webpage Template
+
+1. Click the green "Use this template" button.
+2. Clone your generated repository.
+3. Find and replace all instances of "[Your Name]" with your name.
+
+You can use this template as a starting point and customize it as you see fit.
---
+++ script.js
@@ -0,0 +1,9 @@
+// You can replace the contents of this file with your own JavaScript code.
+
+// For a challenge, why not try adding the current year to the footer?
+// It's currently hard-coded, but this way it would update automatically.
+// You can use new Date().getFullYear() to get the current year.
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/getFullYear
+
+console.log("Hello, World!");
